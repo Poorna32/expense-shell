67ffcf4892e96512757c1a625ce5a65e34642aba
Poorna32 <poornachandraga44@gmail.com>
2024-04-03 16:08:49 +0530

    Adding check status to all the scripts

--- backend.sh
+++ backend.sh
@@ -11,57 +11,57 @@ if [ -z "${mysql_root_password}" ];then
 fi
 
 Print_Task_Heading "disable default nodejs version module"
-dnf module disable nodejs -y  &>>/tmp/expense.log
+dnf module disable nodejs -y  &>>$LOG
 check_status $?
 
 Print_Task_Heading "enable nodejs module for v20"
-dnf module enable nodejs:20 -y &>>/tmp/expense.log
+dnf module enable nodejs:20 -y &>>$LOG
 check_status $?
 
 Print_Task_Heading "Install nodejs"
-dnf install nodejs -y &>>/tmp/expense.log
+dnf install nodejs -y &>>$LOG
 check_status $?
 
 Print_Task_Heading "adding Application User"
-useradd expense &>>/tmp/expense.log
+useradd expense &>>$LOG
 check_status $?
 
 Print_Task_Heading "copy backend Service file"
-cp backend.service /etc/systemd/system/backend.service &>>/tmp/expense.log
+cp backend.service /etc/systemd/system/backend.service &>>$LOG
 check_status $?
 
 Print_Task_Heading "clean the old content"
-rm -rf /app &>>/tmp/expense.log
+rm -rf /app &>>$LOG
 check_status $?
 
 Print_Task_Heading "create app directory"
-mkdir /app &>>/tmp/expense.log
+mkdir /app &>>$LOG
 check_status $?
 
 Print_Task_Heading "Download the app content"
-curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/expense-backend-v2.zip &>>/tmp/expense.log
+curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/expense-backend-v2.zip &>>$LOG
 check_status $?
 
 Print_Task_Heading "Extract App Content"
-cd /app &>>/tmp/expense.log
-unzip /tmp/backend.zip &>>/tmp/expense.log
+cd /app &>>$LOG
+unzip /tmp/backend.zip &>>$LOG
 check_status $?
 
 Print_Task_Heading "Download nodejs dependencies"
-cd /app &>>/tmp/expense.log
-npm install &>>/tmp/expense.log
+cd /app &>>$LOG
+npm install &>>$LOG
 check_status $?
 
 Print_Task_Heading "Start Backend Service"
-systemctl daemon-reload &>>/tmp/expense.log
-systemctl enable backend &>>/tmp/expense.log
-systemctl start backend &>>/tmp/expense.log
+systemctl daemon-reload &>>$LOG
+systemctl enable backend &>>$LOG
+systemctl start backend &>>$LOG
 check_status $?
 
 Print_Task_Heading  "install Mysql Client"
-dnf install mysql -y &>>/tmp/expense.log
+dnf install mysql -y &>>$LOG
 check_status $?
 
 Print_Task_Heading "Load Schema"
-mysql -h 172.31.35.119 -uroot -p${mysql_root_password} < /app/schema/backend.sql &>>/tmp/expense.log
+mysql -h 172.31.35.119 -uroot -p${mysql_root_password} < /app/schema/backend.sql &>>$LOG
 check_status $?
--- mysql.sh
+++ mysql.sh
@@ -1,4 +1,13 @@
-dnf install mysql-server -y
-systemctl enable mysqld
-systemctl start mysqld
-mysql_secure_installation --set-root-pass ExpenseApp@1
+source common.sh
+
+Print_Task_Heading "Install Nginx"
+dnf install mysql-server -y  &>>$LOG
+Check_status $?
+
+Print_Task_Heading "Install Nginx"
+systemctl enable mysqld  &>>$LOG
+systemctl start mysqld  &>>$LOG
+Check_status $?
+
+Print_Task_Heading "Install Nginx"  &>>$LOG
+Check_status $?
