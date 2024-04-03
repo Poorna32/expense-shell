8b9addec2d20c071b5ce17463c876781ba30a29c
Poorna32 <poornachandraga44@gmail.com>
2024-04-03 16:20:39 +0530

    Adding input to my sql server script

--- frontend.sh
+++ frontend.sh
@@ -2,26 +2,26 @@ source common.sh
 
 Print_Task_Heading "Install Nginx"
 dnf install nginx -y &>>$LOG
-Check_status $?
+check_status $?
 
 Print_Task_Heading "Copy Expense Nginx Configuration"
 cp expense.conf /etc/nginx/default.d/expense.conf &>>$LOG
-Check_status $?
+check_status $?
 
 Print_Task_Heading "Clean old content"
 rm -rf /usr/share/nginx/html/* &>>$LOG
-Check_status $?
+check_status $?
 
 Print_Task_Heading "Download App Content"
 curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/expense-frontend-v2.zip &>>$LOG
-Check_status $?
+check_status $?
 
 Print_Task_Heading "Extract App content"
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip &>>$LOG
-Check_status $?
+check_status $?
 
 Print_Task_Heading "Start Nginx Service"
 systemctl enable nginx &>>$LOG
 systemctl restart nginx &>>$LOG
-Check_status $?
+check_status $?
